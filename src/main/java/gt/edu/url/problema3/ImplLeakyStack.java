6a89dd951be540046e43acac793f9a543f5bc14a
APIRO99 <andres99jose@gmail.com>
2017-11-22 08:28:43 -0600

    :(

--- src/main/java/gt/edu/url/problema3/ImplLeakyStack.java
+++ src/main/java/gt/edu/url/problema3/ImplLeakyStack.java
@@ -9,6 +9,57 @@ package gt.edu.url.problema3;
  *
  * @author Laptop
  */
-public class ImplLeakyStack {
+public class ImplLeakyStack<E> implements LeakyStack{
     
+    CircularLinkedList<Object> Ani = new CircularLinkedList<>();
+    int capacidad = 0;
+    int size = 0;
+    
+    public void ImplLeakyStack (int max){
+       capacidad = max; 
+    }
+    @Override
+    public int size() {
+        return size;        
+        throw new UnsupportedOperationException("el tamaño es 0"); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public boolean isEmpty() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object saveHistory(Object e) {
+        if (size == capacidad)
+        {
+            Ani.removeFirst();
+            for (int i = 0; i < Ani.size(); i++) {
+                Ani.first() = Ani.first();
+            }
+        }
+        else
+        {
+                    Ani.addLast(e);
+        }
+        size++;
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object actual() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Object undo() {
+        if (size == 0)
+        {
+            throw new UnsupportedOperationException("No hay historial"); //To change body of generated methods, choose Tools | Templates.
+        }
+        else
+        {
+            
+        }
+    }
 }
